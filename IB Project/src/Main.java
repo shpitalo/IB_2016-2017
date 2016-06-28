4bab137d130d566b037f1c1d51e8c3651cdbc2c4
shpitalo <alyona.shpit@gmail.com>
2016-06-28 14:09:07 -0400

    Add files via upload

---
+++ IB Project/src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Created by alonas on 28/06/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
